08cb7066db2c4275dc604f8d8358d0cddcd37362
Mike P <87105100+MegaZillaGTR@users.noreply.github.com>
2021-07-19 14:40:46 -0400

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -86,9 +86,9 @@ def main():
             foundAllLetters = True  # Start off assumin they've won
             for secretWordLetter in secretWord:
                 if secretWordLetter not in correctLetters:
-                     # There's a letter in the secret word that isn't yet in correctLetters, so the play hasn't won
-                     foundAllLetters = False
-                     break
+                    # There's a letter in the secret word that isn't yet in correctLetters, so the play hasn't won
+                    foundAllLetters = False
+                    break
                 if foundAllLetters:
                     print('Yes! the secret word is: ', secretWord)
                     print('You have won!')
